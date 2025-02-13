48e0276c5923b2ce26562f0bf5875862fbfa521b
skibibidownsyndrome <lupug922@gmail.com>
2025-02-13 09:48:28 -0500

    e

---
+++ Comments.java
@@ -0,0 +1,16 @@
+/************************************
+*Prolect: Demo for comments
+*Programmer: George Lupu
+*Date: 2/11/25
+*Program Name: Comments.java
+*************************************/
+
+
+public class Comments
+{
+public static void main (String[ ] args)
+{
+System.out.println("Hi"); //message to user
+System.out.println("LabOneA"); //assignment
+}
+}
---
+++ ThreeExamples.java
@@ -0,0 +1,11 @@
+public class ThreeExamples {
+    public static void main(String[]args)
+    {
+        System.out.println("Q\"yz89\"Ja4\"tp");
+        System.out.println("Place: \t1\t2\t3\t4\t5\t6\nDog#:\t76\t32\t18\t47\t11\t29");
+        System.out.println("anderson\t3452\nBoyton\t            5344\nCarrington\t3677\nHouston\n               5988\nMcDougal\t5233\nRoberton\t3623\nStryker\t               5112");
+
+
+    }
+    
+}
